e65b0bb05393be5622a31d8d851a2d1036380f76
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2022-02-21 12:30:28 -0500

    Add AVX2 Kernel to tests

--- pennylane_lightning/src/tests/TestKernels.hpp
+++ pennylane_lightning/src/tests/TestKernels.hpp
@@ -15,6 +15,9 @@
 #if PL_USE_AVX512F && PL_USE_AVX512DQ
 #include "cpu_kernels/GateImplementationsAVX512.hpp"
 #endif
+#if PL_USE_AVX2
+#include "cpu_kernels/GateImplementationsAVX2.hpp"
+#endif
 
 using TestKernels =
     Pennylane::Util::TypeList<Pennylane::Gates::GateImplementationsLM,
@@ -24,5 +27,8 @@ using TestKernels =
 #endif
 #if PL_USE_AVX512F && PL_USE_AVX512DQ
                               Pennylane::Gates::GateImplementationsAVX512,
+#endif
+#if PL_USE_AVX2
+                              Pennylane::Gates::GateImplementationsAVX2,
 #endif
                               void>;
